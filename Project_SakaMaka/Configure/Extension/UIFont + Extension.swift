c573cfdd6a3419c75cd60ac5ec703e36ebb2a813
YeoSeongil <seongil_yeo@naver.com>
2024-05-18 19:27:12 +0900

    [ADD] ADD Font

---
+++ Project_SakaMaka/Configure/Extension/UIFont + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIFont + Extension.swift
+//  Project_SakaMaka
+//
+//  Created by 여성일 on 5/18/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let googleLoginButtonFont = UIFont(name: "Roboto-Medium", size: 30.0)
+}
