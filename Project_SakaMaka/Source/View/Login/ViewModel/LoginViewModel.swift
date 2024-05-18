b65ebe7b96362b14a328be600d44a05c70a77a85
YeoSeongil <seongil_yeo@naver.com>
2024-05-18 21:07:21 +0900

    [FEAT] FEAT LoginViewModel

--- Project_SakaMaka/Source/View/Login/ViewModel/LoginViewModel.swift
+++ Project_SakaMaka/Source/View/Login/ViewModel/LoginViewModel.swift
@@ -5,4 +5,36 @@
 //  Created by 여성일 on 5/18/24.
 //
 
-import Foundation
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+protocol LoginViewModelType {
+    // Input
+    var tappedAppleLogin: AnyObserver<Void> { get }
+    var tappedGoogleLogin: AnyObserver<Void> { get }
+}
+
+class LoginViewModel {
+    
+    private let disposeBag = DisposeBag()
+    
+    private let inputTappedAppleLogin = PublishSubject<Void>()
+    private let inputTappedGoogleLogin = PublishSubject<Void>()
+    
+    init() {
+        
+    }
+    
+}
+
+extension LoginViewModel: LoginViewModelType {
+    var tappedAppleLogin: AnyObserver<Void> {
+        inputTappedAppleLogin.asObserver()
+    }
+    
+    var tappedGoogleLogin: AnyObserver<Void> {
+        inputTappedGoogleLogin.asObserver()
+    }
+}
