f6abc0a3f5272da52028c04c78be931973943ee8
YeoSeongil <seongil_yeo@naver.com>
2024-05-18 23:25:47 +0900

    [FEAT] FEAT FireBaseService

--- Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
+++ Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
@@ -5,4 +5,31 @@
 //  Created by 여성일 on 5/18/24.
 //
 
-import Foundation
+import UIKit
+
+import FirebaseAuth
+import FirebaseFirestore
+
+import RxSwift
+
+class FireBaseService {
+    
+    static let shared = FireBaseService()
+    
+    private init() { }
+    
+    func checkIsCurrentUserRegistered() -> Observable<Bool> {
+        return Observable.create { observer in
+            if let currentUser = Auth.auth().currentUser {
+                Firestore.firestore().collection("users").document(currentUser.uid).getDocument { doc, error in
+                    if let doc = doc, doc.exists {
+                        observer.onNext(true)
+                    } else {
+                        observer.onNext(false)
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
+}
