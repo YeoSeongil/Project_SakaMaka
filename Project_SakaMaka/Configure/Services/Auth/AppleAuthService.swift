cfd8e5f010178d8ff2406b0e8d75c09daccf4568
YeoSeongil <seongil_yeo@naver.com>
2024-05-18 23:26:51 +0900

    [FEAT] FEAT AppleAuthService

--- Project_SakaMaka/Configure/Services/Auth/AppleAuthService.swift
+++ Project_SakaMaka/Configure/Services/Auth/AppleAuthService.swift
@@ -5,4 +5,113 @@
 //  Created by 여성일 on 5/18/24.
 //
 
-import Foundation
+import UIKit
+import AuthenticationServices
+import CryptoKit
+import FirebaseAuth
+
+protocol AppleAuthServiceDelegate: AnyObject {
+    func didSuccessSignInFirebaseWithApple()
+    func didFailedSignInFirebaseWithApple(_ error: Error)
+    func didFailedAppleLoginHandle(_ error: Error)
+}
+
+class AppleAuthService: NSObject {
+    
+    static let shared = AppleAuthService()
+    
+    weak var delegate: AppleAuthServiceDelegate?
+    private var currentNonce: String?
+    
+    private override init() {}
+    
+    func signInWithApple() {
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+    }
+    
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            String(format: "%02x", $0)
+        }.joined()
+
+        return hashString
+    }
+
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        var randomBytes = [UInt8](repeating: 0, count: length)
+        let errorCode = SecRandomCopyBytes(kSecRandomDefault, randomBytes.count, &randomBytes)
+        if errorCode != errSecSuccess {
+            fatalError(
+                "Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)"
+            )
+        }
+
+        let charset: [Character] =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+
+        let nonce = randomBytes.map { byte in
+            charset[Int(byte) % charset.count]
+        }
+
+        return String(nonce)
+    }
+}
+
+extension AppleAuthService: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        guard let window = UIApplication.shared.windows.first else {
+            fatalError("No windows found")
+        }
+        return window
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+
+            let credential = OAuthProvider.appleCredential(withIDToken: idTokenString,
+                                                            rawNonce: nonce,
+                                                            fullName: appleIDCredential.fullName)
+            
+            Auth.auth().signIn(with: credential) { [weak self] (authResult, error) in
+                if let error = error {
+                    self?.delegate?.didFailedSignInFirebaseWithApple(error)
+                    return
+                }
+                
+                if let authResult = authResult {
+                    self?.delegate?.didSuccessSignInFirebaseWithApple()
+                }
+            }
+        }
+    }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Sign in with Apple errored: \(error)")
+        delegate?.didFailedAppleLoginHandle(error)
+    }
+}
