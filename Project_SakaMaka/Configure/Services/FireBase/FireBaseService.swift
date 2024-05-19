8927c56eac332b746d451143920eb18de238499d
YeoSeongil <seongil_yeo@naver.com>
2024-05-19 16:53:06 +0900

    [FEAT] FEAT FireBaseService

--- Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
+++ Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
@@ -12,20 +12,54 @@ import FirebaseFirestore
 
 import RxSwift
 
+enum FirebaseError: Error {
+    case notFindCurrentUser
+    case failedRegister
+}
+
+enum AuthResult {
+    case success
+    case failed
+}
+
 class FireBaseService {
     
     static let shared = FireBaseService()
     
     private init() { }
     
-    func checkIsCurrentUserRegistered() -> Observable<Bool> {
+    // <Result<AuthResult, FirebaseError>>
+    func checkIsCurrentUserRegistered() -> Observable<checkIsCurrentUserRegisteredType> {
         return Observable.create { observer in
             if let currentUser = Auth.auth().currentUser {
                 Firestore.firestore().collection("users").document(currentUser.uid).getDocument { doc, error in
                     if let doc = doc, doc.exists {
-                        observer.onNext(true)
+                        observer.onNext(.success)
                     } else {
-                        observer.onNext(false)
+                        observer.onNext(.notFindCurrentUser)
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
+    
+    func registerFirebaseUser(nickName: String) -> Observable<registerFirebaseUserType> {
+        return Observable.create { observer in
+            if let currentUser = Auth.auth().currentUser {
+                let userRef = Firestore.firestore().collection("users").document(currentUser.uid)
+                
+                let userData: [String: Any] = [
+                    "name" : nickName,
+                    "image" : "imageURL",
+                    "isRegistered" : true
+                ]
+                
+                userRef.setData(userData) { error in
+                    if let error = error {
+                        observer.onNext(.failedRegister)
+                    }  else {
+                        observer.onNext(.success)
                     }
                 }
             }
