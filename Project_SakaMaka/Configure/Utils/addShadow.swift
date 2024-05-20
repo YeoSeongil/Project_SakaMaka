3fbc968cec5d89bc1a72abe240cabdb740dbf136
YeoSeongil <seongil_yeo@naver.com>
2024-05-20 19:04:44 +0900

    [FEAT] FEAT addShadow

--- Project_SakaMaka/Configure/Utils/addShadow.swift
+++ Project_SakaMaka/Configure/Utils/addShadow.swift
@@ -5,4 +5,28 @@
 //  Created by 여성일 on 5/20/24.
 //
 
-import Foundation
+import UIKit
+
+extension CALayer {
+    func addShadow(
+        color: UIColor = .black,
+        alpha: Float = 0.5,
+        x: CGFloat = 0,
+        y: CGFloat = 2,
+        blur: CGFloat = 4,
+        spread: CGFloat = 0) {
+            
+            masksToBounds = false
+            shadowColor = color.cgColor
+            shadowOpacity = alpha
+            shadowOffset = CGSize(width: x, height: y)
+            shadowRadius = blur / 2.0
+            if spread == 0 {
+                shadowPath = nil
+            } else {
+                let dx = -spread
+                let rect = bounds.insetBy(dx: dx, dy: dx)
+                shadowPath = UIBezierPath(rect: rect).cgPath
+            }
+        }
+}
