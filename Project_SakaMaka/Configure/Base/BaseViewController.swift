082466c2468aab34567210346ed67b23a6792e3c
YeoSeongil <seongil_yeo@naver.com>
2024-05-20 14:48:38 +0900

    [CHORE] CHORE BaseViewController

--- Project_SakaMaka/Configure/Base/BaseViewController.swift
+++ Project_SakaMaka/Configure/Base/BaseViewController.swift
@@ -15,7 +15,7 @@ class BaseViewController: UIViewController {
     
     override func viewDidLoad() {
             super.viewDidLoad()
-            self.view.backgroundColor = .black
+            self.view.backgroundColor = .white
             setViewController()
             setConstraints()
             setNavigation()
