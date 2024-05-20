1cdedcfd7f59355c806df2c09dc74151dc51ccce
YeoSeongil <seongil_yeo@naver.com>
2024-05-20 13:09:11 +0900

    [RENAME] RENAME RegisterViewController

---
+++ Project_SakaMaka/Source/View/Register/ViewController/RegisterViewController.swift
@@ -0,0 +1,26 @@
+//
+//  RegisterViewController.swift
+//  Project_SakaMaka
+//
+//  Created by 여성일 on 5/20/24.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+import SnapKit
+import Then
+
+class RegisterViewController: BaseViewController {
+
+    private let viewModel: LoginViewModelType
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
