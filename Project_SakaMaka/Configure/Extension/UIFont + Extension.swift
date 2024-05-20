c998e924336bcdd5a01f55db9a11b909bc51335c
YeoSeongil <seongil_yeo@naver.com>
2024-05-20 13:43:02 +0900

    [FEAT] FEAT UIFont + Extension

--- Project_SakaMaka/Configure/Extension/UIFont + Extension.swift
+++ Project_SakaMaka/Configure/Extension/UIFont + Extension.swift
@@ -8,5 +8,83 @@
 import UIKit
 
 extension UIFont {
-    static let googleLoginButtonFont = UIFont(name: "Roboto-Medium", size: 30.0)
+    static func setFont(_ style: Pretendard, ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: style.rawValue, size: size)!
+    }
 }
+
+enum Pretendard: String {
+    case pretendardBold = "Pretendard-Bold"
+    case pretendardSemiBold = "Pretendard-SemiBold"
+    case pretendardMedium = "Pretendard-Medium"
+    case pretendardRegular = "Pretendard-Regular"
+}
+
+extension UIFont {
+    @nonobjc class var h1: UIFont {
+        return UIFont.setFont(.pretendardBold, ofSize: 24)
+    }
+    
+    @nonobjc class var h2: UIFont {
+        return UIFont.setFont(.pretendardBold, ofSize: 22)
+    }
+    
+    @nonobjc class var h2_2: UIFont {
+        return UIFont.setFont(.pretendardMedium, ofSize: 22)
+    }
+    
+    @nonobjc class var h3: UIFont {
+        return UIFont.setFont(.pretendardBold, ofSize: 20)
+    }
+    
+    @nonobjc class var h4: UIFont {
+        return UIFont.setFont(.pretendardBold, ofSize: 18)
+    }
+    
+    @nonobjc class var h5: UIFont {
+        return UIFont.setFont(.pretendardSemiBold, ofSize: 15)
+    }
+    
+    @nonobjc class var h6: UIFont {
+        return UIFont.setFont(.pretendardSemiBold, ofSize: 160)
+    }
+    
+    @nonobjc class var b1: UIFont {
+        return UIFont.setFont(.pretendardMedium, ofSize: 17)
+    }
+    
+    @nonobjc class var b2: UIFont {
+        return UIFont.setFont(.pretendardMedium, ofSize: 15)
+    }
+    
+    @nonobjc class var b3: UIFont {
+        return UIFont.setFont(.pretendardRegular, ofSize: 15)
+    }
+    
+    @nonobjc class var b4: UIFont {
+        return UIFont.setFont(.pretendardMedium, ofSize: 14)
+    }
+    
+    @nonobjc class var b5: UIFont {
+        return UIFont.setFont(.pretendardSemiBold, ofSize: 13)
+    }
+    
+    @nonobjc class var b6: UIFont {
+        return UIFont.setFont(.pretendardRegular, ofSize: 13)
+    }
+    
+    @nonobjc class var b7: UIFont {
+        return UIFont.setFont(.pretendardMedium, ofSize: 12)
+    }
+    
+    @nonobjc class var b8: UIFont {
+        return UIFont.setFont(.pretendardRegular, ofSize: 12)
+    }
+    
+    @nonobjc class var b9: UIFont {
+        return UIFont.setFont(.pretendardSemiBold, ofSize: 11)
+    }
+}
+
+
+
