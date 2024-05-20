45966c37db06513669a2f416ee905ad2dceb4052
YeoSeongil <seongil_yeo@naver.com>
2024-05-20 14:48:28 +0900

    [CHORE] CHORE UIFont + Extension

--- Project_SakaMaka/Configure/Extension/UIFont + Extension.swift
+++ Project_SakaMaka/Configure/Extension/UIFont + Extension.swift
@@ -8,82 +8,33 @@
 import UIKit
 
 extension UIFont {
-    static func setFont(_ style: Pretendard, ofSize size: CGFloat) -> UIFont {
-        return UIFont(name: style.rawValue, size: size)!
+    enum Pretendard: String {
+        case pretendardBold = "Pretendard-Bold"
+        case pretendardSemiBold = "Pretendard-SemiBold"
+        case pretendardMedium = "Pretendard-Medium"
+        case pretendardRegular = "Pretendard-Regular"
     }
-}
-
-enum Pretendard: String {
-    case pretendardBold = "Pretendard-Bold"
-    case pretendardSemiBold = "Pretendard-SemiBold"
-    case pretendardMedium = "Pretendard-Medium"
-    case pretendardRegular = "Pretendard-Regular"
 }
 
 extension UIFont {
-    @nonobjc class var h1: UIFont {
-        return UIFont.setFont(.pretendardBold, ofSize: 24)
-    }
     
-    @nonobjc class var h2: UIFont {
-        return UIFont.setFont(.pretendardBold, ofSize: 22)
-    }
+    static let h1 = UIFont(name: Pretendard.pretendardBold.rawValue, size: 24)!
+    static let h2 = UIFont(name: Pretendard.pretendardBold.rawValue, size: 22)!
+    static let h2_2 = UIFont(name: Pretendard.pretendardMedium.rawValue, size: 22)!
+    static let h3 = UIFont(name: Pretendard.pretendardBold.rawValue, size: 20)!
+    static let h4 = UIFont(name: Pretendard.pretendardBold.rawValue, size: 18)!
+    static let h5 = UIFont(name: Pretendard.pretendardBold.rawValue, size: 15)!
     
-    @nonobjc class var h2_2: UIFont {
-        return UIFont.setFont(.pretendardMedium, ofSize: 22)
-    }
+    static let b1 = UIFont(name: Pretendard.pretendardMedium.rawValue, size: 17)!
+    static let b2 = UIFont(name: Pretendard.pretendardMedium.rawValue, size: 15)!
+    static let b3 = UIFont(name: Pretendard.pretendardRegular.rawValue, size: 15)!
+    static let b4 = UIFont(name: Pretendard.pretendardMedium.rawValue, size: 14)!
+    static let b5 = UIFont(name: Pretendard.pretendardSemiBold.rawValue, size: 13)!
+    static let b6 = UIFont(name: Pretendard.pretendardRegular.rawValue, size: 13)!
+    static let b7 = UIFont(name: Pretendard.pretendardMedium.rawValue, size: 12)!
+    static let b8 = UIFont(name: Pretendard.pretendardRegular.rawValue, size: 12)!
+    static let b9 = UIFont(name: Pretendard.pretendardRegular.rawValue, size: 11)!
     
-    @nonobjc class var h3: UIFont {
-        return UIFont.setFont(.pretendardBold, ofSize: 20)
-    }
-    
-    @nonobjc class var h4: UIFont {
-        return UIFont.setFont(.pretendardBold, ofSize: 18)
-    }
-    
-    @nonobjc class var h5: UIFont {
-        return UIFont.setFont(.pretendardSemiBold, ofSize: 15)
-    }
-    
-    @nonobjc class var h6: UIFont {
-        return UIFont.setFont(.pretendardSemiBold, ofSize: 160)
-    }
-    
-    @nonobjc class var b1: UIFont {
-        return UIFont.setFont(.pretendardMedium, ofSize: 17)
-    }
-    
-    @nonobjc class var b2: UIFont {
-        return UIFont.setFont(.pretendardMedium, ofSize: 15)
-    }
-    
-    @nonobjc class var b3: UIFont {
-        return UIFont.setFont(.pretendardRegular, ofSize: 15)
-    }
-    
-    @nonobjc class var b4: UIFont {
-        return UIFont.setFont(.pretendardMedium, ofSize: 14)
-    }
-    
-    @nonobjc class var b5: UIFont {
-        return UIFont.setFont(.pretendardSemiBold, ofSize: 13)
-    }
-    
-    @nonobjc class var b6: UIFont {
-        return UIFont.setFont(.pretendardRegular, ofSize: 13)
-    }
-    
-    @nonobjc class var b7: UIFont {
-        return UIFont.setFont(.pretendardMedium, ofSize: 12)
-    }
-    
-    @nonobjc class var b8: UIFont {
-        return UIFont.setFont(.pretendardRegular, ofSize: 12)
-    }
-    
-    @nonobjc class var b9: UIFont {
-        return UIFont.setFont(.pretendardSemiBold, ofSize: 11)
-    }
 }
 
 
