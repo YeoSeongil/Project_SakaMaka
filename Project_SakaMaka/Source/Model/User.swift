682525e4bd87600ccbfd067b51fbd5ae23990b04
YeoSeongil <seongil_yeo@naver.com>
2024-05-21 17:02:36 +0900

    [FEAT] FEAT User

--- Project_SakaMaka/Source/Model/User.swift
+++ Project_SakaMaka/Source/Model/User.swift
@@ -5,4 +5,16 @@
 //  Created by 여성일 on 5/21/24.
 //
 
-import Foundation
+import FirebaseFirestore
+
+struct User: Codable {
+    var id: String?  // Firestore 문서 ID를 저장
+    var name: String
+    var isRegistered: Bool
+    
+    init(id: String? = nil, name: String, profileImageURL: String, isRegistered: Bool) {
+        self.id = id
+        self.name = name
+        self.isRegistered = isRegistered
+    }
+}
