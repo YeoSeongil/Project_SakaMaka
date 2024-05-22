e739b8cec663f9137bdcd06d841afdc5ae0ed07d
YeoSeongil <seongil_yeo@naver.com>
2024-05-22 22:45:26 +0900

    [FEAT] FEAT AddVoteSelectImageView

--- Project_SakaMaka/Source/View/AddVote/Components/AddVoteSelectImageView.swift
+++ Project_SakaMaka/Source/View/AddVote/Components/AddVoteSelectImageView.swift
@@ -7,14 +7,85 @@
 
 import UIKit
 
-class AddVoteSelectImageView: UIView {
+import RxCocoa
+import RxSwift
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+import SnapKit
+import Then
+import PhotosUI
 
+protocol AddVoteSelectImageViewDelegate: AnyObject {
+    func didSelectImageButtonTapped()
 }
+
+class AddVoteSelectImageView: UIView {
+    
+    private let disposeBag = DisposeBag()
+    weak var delegate: AddVoteSelectImageViewDelegate?
+    
+    // MARK: - UI Components
+    private let titleDescriptionLabel = UILabel().then {
+        let attributedString = NSMutableAttributedString(string: "상품의 사진을 등록해주세요.", attributes: [.font: UIFont.b2, .foregroundColor: UIColor.black])
+        attributedString.append(NSAttributedString(string: " (선택사항)", attributes: [.font: UIFont.b5, .foregroundColor: UIColor.nightGray]))
+        $0.attributedText = attributedString
+        $0.backgroundColor = .clear
+    }
+    
+    private lazy var selectImageButton = UIButton().then {
+        $0.backgroundColor = .clear
+        $0.setImage(UIImage(named: "selectImage")?.withRenderingMode(.alwaysTemplate), for: .normal)
+        $0.imageView?.layer.cornerRadius = 10
+        $0.tintColor = .nightGray
+        $0.layer.cornerRadius = 10
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.nightGray.cgColor
+    }
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setView()
+        setConstraint()
+        bind()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - SetUp View
+    private func setView() {
+        backgroundColor = .clear
+        
+        [titleDescriptionLabel, selectImageButton].forEach { addSubview($0) }
+    }
+    
+    private func setConstraint() {
+        titleDescriptionLabel.snp.makeConstraints {
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        selectImageButton.snp.makeConstraints {
+            $0.top.equalTo(titleDescriptionLabel.snp.bottom).offset(5)
+            $0.height.equalTo(170)
+            $0.horizontalEdges.equalToSuperview()
+        }
+    }
+    
+    private func bind() {
+        selectImageButton.rx.tap
+            .subscribe(with: self, onNext: { owner, _ in
+                owner.delegate?.didSelectImageButtonTapped()
+            })
+            .disposed(by: disposeBag)
+    }
+}
+
+// MARK: - Method
+extension AddVoteSelectImageView {
+    func setImage(_ image: UIImage) {
+        selectImageButton.setImage(image, for: .normal)
+        selectImageButton.layer.borderWidth = 0
+    }
+}
+
