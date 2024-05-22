3e64472e87b10a64271b16e2d76f904f9d237e02
YeoSeongil <seongil_yeo@naver.com>
2024-05-22 22:45:36 +0900

    [FEAT] FEAT AddVoteViewController

--- Project_SakaMaka/Source/View/AddVote/ViewController/AddVoteViewController.swift
+++ Project_SakaMaka/Source/View/AddVote/ViewController/AddVoteViewController.swift
@@ -13,17 +13,40 @@ import RxSwift
 import SnapKit
 import Then
 
+import PhotosUI
+
 class AddVoteViewController: BaseViewController {
 
     // MARK: - UI Components
+    private lazy var headerView = AddVoteHeaderView().then {
+        $0.delegate = self
+    }
+    
+    private let addVoteTitleView = AddVoteTitleView()
+    private lazy var addVoteSelectImageView = AddVoteSelectImageView().then {
+        $0.delegate = self
+    }
+    
     private let stackView = UIStackView().then {
         $0.axis = .vertical
+        $0.spacing = 10
+        $0.alignment = .fill
+        $0.distribution = .fillEqually
         $0.backgroundColor = .clear
     }
     
     private lazy var scrollView = UIScrollView().then {
         $0.backgroundColor = .clear
         $0.addSubview(stackView)
+    }
+
+    private let addButton = UIButton().then {
+        $0.setTitle("등록하기", for: .normal)
+        $0.titleLabel?.textColor = .white
+        $0.titleLabel?.textAlignment = .center
+        $0.titleLabel?.font = .h4
+        $0.backgroundColor = .Turquoise
+        $0.layer.cornerRadius = 10
     }
     
     // MARK: - Init
@@ -37,14 +60,94 @@ class AddVoteViewController: BaseViewController {
     
     // MARK: - SetUp VC
     override func setViewController() {
-        [scrollView].forEach {
+        [headerView, scrollView, addButton].forEach {
             view.addSubview($0)
+        }
+        
+        [addVoteTitleView, addVoteSelectImageView].forEach {
+            stackView.addArrangedSubview($0)
         }
     }
     
     override func setConstraints() {
+        headerView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.horizontalEdges.equalToSuperview()
+            $0.height.equalTo(32)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.width.equalToSuperview()
+            $0.height.equalToSuperview()
+        }
+        
         scrollView.snp.makeConstraints {
+            $0.top.equalTo(headerView.snp.bottom).offset(25)
+            $0.bottom.equalTo(addButton.snp.top).offset(25)
             $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        addButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(10)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(50)
+        }
+    }
+}
+
+// MARK: - Method
+extension AddVoteViewController {
+    private func presentImagePicker() {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 1
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = self
+        present(picker, animated: true, completion: nil)
+    }
+}
+
+extension AddVoteViewController: AddVoteHeaderViewDelegate {
+    func didBackbuttonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
+
+extension AddVoteViewController: AddVoteSelectImageViewDelegate {
+    func didSelectImageButtonTapped() {
+        presentImagePicker()
+    }
+    
+    func didSelectImageTapped() {
+        
+    }
+}
+
+extension AddVoteViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let result = results.first else { return }
+        
+        result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (object, error) in
+            if let error = error {
+                print("Error loading image: \(error.localizedDescription)")
+                return
+            }
+            
+            result.itemProvider.loadFileRepresentation(forTypeIdentifier: UTType.image.identifier) { [weak self] (url, error) in
+                if let error = error {
+                    print("Error loading image URL: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let image = object as? UIImage else { return }
+                
+                DispatchQueue.main.async {
+                    self?.addVoteSelectImageView.setImage(image)
+                }
+            }
         }
     }
 }
