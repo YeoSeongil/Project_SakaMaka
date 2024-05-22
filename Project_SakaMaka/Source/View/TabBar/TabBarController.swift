f1422ea0c9c213b6e082a25cfa5cfa6ffb02bfd4
YeoSeongil <seongil_yeo@naver.com>
2024-05-22 19:03:26 +0900

    [CHORE] CHORE TabBarController

--- Project_SakaMaka/Source/View/TabBar/TabBarController.swift
+++ Project_SakaMaka/Source/View/TabBar/TabBarController.swift
@@ -30,8 +30,8 @@ class TabBarController: UITabBarController {
     
     private func setTabBarController() {
         // Todo 나머지 VC 구현 후 추가
-        let homeVC = setNavigationController(title: "홈", unselectedImage: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"), rootViewController: HomeViewController())
-        let testVC2 = setNavigationController(title: "홈2", unselectedImage: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "home.fill"), rootViewController: Test2ViewController())
+        let homeVC = setNavigationController(title: "홈", unselectedImage: UIImage(systemName: "house"), rootViewController: HomeViewController())
+        let testVC2 = setNavigationController(title: "피드", unselectedImage: UIImage(named:  "tapbar_feed"), rootViewController: FeedViewController())
         
         viewControllers = [homeVC, testVC2]
     }
@@ -39,11 +39,10 @@ class TabBarController: UITabBarController {
 
 // MARK: - Method
 extension TabBarController {
-    private func setNavigationController(title: String, unselectedImage: UIImage?, selectedImage: UIImage?, rootViewController: UIViewController) -> UINavigationController {
+    private func setNavigationController(title: String, unselectedImage: UIImage?, rootViewController: UIViewController) -> UINavigationController {
         let navigationController = UINavigationController(rootViewController: rootViewController)
         navigationController.title = title
         navigationController.tabBarItem.image = unselectedImage
-        navigationController.tabBarItem.selectedImage = selectedImage
         navigationController.navigationBar.isHidden = true
         return navigationController
     }
