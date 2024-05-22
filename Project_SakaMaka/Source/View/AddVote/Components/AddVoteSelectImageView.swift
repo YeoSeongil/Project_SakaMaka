aad9f8ddff823caedfa5714c9b5bb06150a12249
YeoSeongil <seongil_yeo@naver.com>
2024-05-22 21:46:04 +0900

    [ADD] ADDVoteSelectImageView

---
+++ Project_SakaMaka/Source/View/AddVote/Components/AddVoteSelectImageView.swift
@@ -0,0 +1,20 @@
+//
+//  AddVoteSelectImageView.swift
+//  Project_SakaMaka
+//
+//  Created by 여성일 on 5/22/24.
+//
+
+import UIKit
+
+class AddVoteSelectImageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
