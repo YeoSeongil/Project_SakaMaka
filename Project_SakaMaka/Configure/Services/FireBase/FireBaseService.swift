88ffcc23aec57a84d5b4d015220626458aaadb52
YeoSeongil <seongil_yeo@naver.com>
2024-05-23 21:44:29 +0900

    [FEAT] FEAT FireBaseService

--- Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
+++ Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
@@ -62,4 +62,68 @@ class FireBaseService {
             return Disposables.create()
         }
     }
+    
+    func postUpload(title: String, image: UIImage, price: String, link: String, content: String) -> Observable<postUploadType> {
+        return Observable.create { observer in
+            if let currentUser = Auth.auth().currentUser {
+                // 이미지 업로드
+                guard let imageData = image.jpegData(compressionQuality: 0.5) else { return Disposables.create()  }
+                
+                let imageName = "\(title)\(UUID().uuidString)"
+                let storageRef = Storage.storage().reference().child("postImages/\(imageName)")
+                
+                storageRef.putData(imageData, metadata: nil) { metadata, error in
+                    if let error = error {
+                        // Handle error
+                    } else {
+                        // 이미지 다운로드 URL 획득
+                        storageRef.downloadURL { url, error in
+                            if let error = error {
+                                observer.onNext(.handleError)
+                            } else if let url = url {
+                                // 프로필 이미지 다운로드 URL 획득
+                                let profileImageStorageRef = Storage.storage().reference().child("profiles/\(currentUser.uid)")
+                                profileImageStorageRef.downloadURL { profileURL, error in
+                                    if let error = error {
+                                        observer.onNext(.handleError)
+                                    } else if let profileURL = profileURL {
+                                        // 사용자 이름 가져오기
+                                        Firestore.firestore().collection("users").document(currentUser.uid).getDocument { (document, error) in
+                                            if let error = error {
+                                                observer.onNext(.handleError)
+                                            } else if let document = document, document.exists, let userName = document.data()?["name"] as? String {
+                                                // 이미지 URL과 함께 포스트 데이터 Firestore에 저장
+                                                let postData: [String: Any] = [
+                                                    "title": title,
+                                                    "imageURL": url.absoluteString,
+                                                    "profileURL": profileURL.absoluteString,
+                                                    "price": price,
+                                                    "link": link,
+                                                    "content": content,
+                                                    "authorName": userName,
+                                                    "authorID": currentUser.uid,
+                                                    "timestamp": FieldValue.serverTimestamp()
+                                                ]
+                                                
+                                                Firestore.firestore().collection("posts").addDocument(data: postData) { error in
+                                                    if let error = error {
+                                                        observer.onNext(.handleError)
+                                                    } else {
+                                                        observer.onNext(.success)
+                                                    }
+                                                }
+                                            } else {
+                                                observer.onNext(.handleError)
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return Disposables.create()
+        }
+    }
 }
