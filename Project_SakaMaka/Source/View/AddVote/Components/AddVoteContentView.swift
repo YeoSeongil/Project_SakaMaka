692070963953f6fb1293cfb9881568e9fbd13429
YeoSeongil <seongil_yeo@naver.com>
2024-05-23 19:03:17 +0900

    [CHORE] CHORE AddVoteContentView

--- Project_SakaMaka/Source/View/AddVote/Components/AddVoteContentView.swift
+++ Project_SakaMaka/Source/View/AddVote/Components/AddVoteContentView.swift
@@ -31,10 +31,12 @@ class AddVoteContentView: UIView {
     
     private let contentTextView = UITextView().then {
         $0.font = .b4
-        $0.textColor = .Turquoise
         $0.layer.cornerRadius = 10
         $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.black.cgColor
+        $0.layer.borderColor = UIColor.nightGray.cgColor
+        $0.textContainerInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        $0.text = "ex) 아이패드 9.7인치 살까? 아니면 12.9인치 살까?"
+        $0.textColor = .nightGray
     }
     
     // MARK: - Init
@@ -64,23 +66,35 @@ class AddVoteContentView: UIView {
         
         contentTextView.snp.makeConstraints {
             $0.top.equalTo(contentDescriptionLabel.snp.bottom).offset(5)
-            $0.height.equalTo(150)
+            $0.height.equalTo(120)
             $0.horizontalEdges.equalToSuperview()
         }
     }
     
     private func bind() {
         contentTextView.rx.text.orEmpty
-            .map { $0.count <= 8 }
+            .map { $0.count <= 100 }
             .subscribe(with: self, onNext: { owner, isEditable in
                 owner.characterLimitTitleTextField(isEditable)
             })
             .disposed(by: disposeBag)
         
-        contentTextView.rx.text.orEmpty
-            .map { $0.isEmpty }
-            .subscribe(with: self, onNext: { owner, isEmpty in
-                owner.changeContentTextFieldLayerColor(isEmpty)
+//        contentTextView.rx.text.orEmpty
+//            .map { $0.isEmpty }
+//            .subscribe(with: self, onNext: { owner, isEmpty in
+//                owner.changeContentTextFieldLayerColor(isEmpty)
+//            })
+//            .disposed(by: disposeBag)
+        
+        contentTextView.rx.didBeginEditing
+            .subscribe(with: self, onNext: { owner, _ in
+                owner.didBeginEditingEvent()
+            })
+            .disposed(by: disposeBag)
+        
+        contentTextView.rx.didEndEditing
+            .subscribe(with: self, onNext: { owner, _ in
+                owner.didEndEditingEvent()
             })
             .disposed(by: disposeBag)
     }
@@ -96,5 +110,21 @@ extension AddVoteContentView {
     
     private func changeContentTextFieldLayerColor(_ isEmpty: Bool) {
         contentTextView.layer.borderColor = isEmpty ? UIColor.nightGray.cgColor : UIColor.Turquoise.cgColor
+    }
+    
+    private func didBeginEditingEvent() {
+        if contentTextView.textColor == UIColor.nightGray {
+            contentTextView.text = nil
+            contentTextView.textColor = UIColor.Turquoise
+            contentTextView.layer.borderColor = UIColor.Turquoise.cgColor
+        }
+    }
+    
+    private func didEndEditingEvent() {
+        if contentTextView.text.isEmpty {
+            contentTextView.text = "ex) 아이패드 9.7인치 살까? 아니면 12.9인치 살까?"
+            contentTextView.textColor = UIColor.nightGray
+            contentTextView.layer.borderColor = UIColor.nightGray.cgColor
+        }
     }
 }
