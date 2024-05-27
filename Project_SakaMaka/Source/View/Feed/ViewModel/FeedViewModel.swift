9e19f6956c222e76ff360c1337a4a2335ba834d4
YeoSeongil <seongil_yeo@naver.com>
2024-05-27 20:39:44 +0900

    [FEAT] FEAT FeedViewModel

--- Project_SakaMaka/Source/View/Feed/ViewModel/FeedViewModel.swift
+++ Project_SakaMaka/Source/View/Feed/ViewModel/FeedViewModel.swift
@@ -5,4 +5,45 @@
 //  Created by 여성일 on 5/27/24.
 //
 
-import Foundation
+import RxCocoa
+import RxSwift
+
+import FirebaseFirestore
+
+protocol FeedViewModelType {
+    // Output
+    var postsData: Driver<[Post]> { get }
+}
+
+class FeedViewModel {
+    
+    private let postsOutput = BehaviorRelay<[Post]>(value: [])
+    
+    init() {
+        fetchPosts()
+    }
+    
+    func fetchPosts() {
+        Firestore.firestore().collection("posts").addSnapshotListener { [weak self] (snapshot, error) in
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("error")
+            }
+            
+            guard let documents = snapshot?.documents else {
+                print("no doc")
+                return
+            }
+
+            let postsArray = documents.compactMap { Post(document: $0.data()) }
+            self.postsOutput.accept(postsArray)
+        }
+    }
+}
+
+extension FeedViewModel: FeedViewModelType {
+    var postsData: Driver<[Post]> {
+        postsOutput.asDriver(onErrorDriveWith: .empty())
+    }
+}
