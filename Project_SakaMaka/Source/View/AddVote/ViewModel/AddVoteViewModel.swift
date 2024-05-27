e15b23602386a1813d92c22c02a571986167f39b
YeoSeongil <seongil_yeo@naver.com>
2024-05-27 21:52:15 +0900

    [FEAT] FEAT AddVoteViewModel

--- Project_SakaMaka/Source/View/AddVote/ViewModel/AddVoteViewModel.swift
+++ Project_SakaMaka/Source/View/AddVote/ViewModel/AddVoteViewModel.swift
@@ -9,12 +9,16 @@ import RxCocoa
 import RxSwift
 
 protocol AddVoteViewModelType {
+    // Input
     var postButtonTapped: AnyObserver<Void> { get }
     var titleValue: AnyObserver<String> { get }
     var imageValue: AnyObserver<UIImage> { get }
     var priceValue: AnyObserver<String> { get }
     var linkValue: AnyObserver<String> { get }
     var contentValue: AnyObserver<String> { get }
+    
+    // Output
+    var postUploadResult: Driver<postUploadType> { get }
 }
 
 class AddVoteViewModel {
@@ -29,25 +33,29 @@ class AddVoteViewModel {
     private let inputLinkValue = PublishSubject<String>()
     private let inputContentValue = PublishSubject<String>()
     
+    // Output
+    private let outputPostUploadResult = PublishRelay<postUploadType>()
+    
     init() {
         inputPostButtonTapped
             .withLatestFrom(Observable.combineLatest(inputTitleValue, inputImageValue, inputPriceValue, inputLinkValue, inputContentValue))
             .subscribe(with: self, onNext: { owner, value in
                 owner.firebaseService.postUpload(title: value.0, image: value.1, price: value.2, link: value.3, content: value.4)
-                    .subscribe(onNext: { result in
+                    .subscribe(with:self, onNext: { owner, result in
                         switch result {
                         case .success:
-                            print("성공")
+                            owner.outputPostUploadResult.accept(.success)
                         case .handleError:
-                            print("실패")
+                            owner.outputPostUploadResult.accept(.handleError)
                         }
-                    }).disposed(by: self.disposeBag)
+                    }).disposed(by: owner.disposeBag)
             })
             .disposed(by: disposeBag)
     }
 }
 
 extension AddVoteViewModel: AddVoteViewModelType {
+    // Input
     var postButtonTapped: AnyObserver<Void> {
         inputPostButtonTapped.asObserver()
     }
@@ -70,5 +78,10 @@ extension AddVoteViewModel: AddVoteViewModelType {
     
     var contentValue: AnyObserver<String> {
         inputContentValue.asObserver()
+    }
+    
+    // Output
+    var postUploadResult: Driver<postUploadType> {
+        outputPostUploadResult.asDriver(onErrorDriveWith: .empty())
     }
 }
