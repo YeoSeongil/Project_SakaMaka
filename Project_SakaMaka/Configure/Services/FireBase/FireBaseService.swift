cb9660026961fe57dc95dbcdc958529788e9908d
YeoSeongil <seongil_yeo@naver.com>
2024-05-28 22:24:09 +0900

    [FEAT] FEAT FireBaseService

--- Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
+++ Project_SakaMaka/Configure/Services/FireBase/FireBaseService.swift
@@ -67,14 +67,17 @@ class FireBaseService {
         return Observable.create { observer in
             if let currentUser = Auth.auth().currentUser {
                 // 이미지 업로드
-                guard let imageData = image.jpegData(compressionQuality: 0.5) else { return Disposables.create()  }
+                guard let imageData = image.jpegData(compressionQuality: 0.5) else {
+                    observer.onNext(.handleError)
+                    return Disposables.create()
+                }
                 
                 let imageName = "\(title)\(UUID().uuidString)"
                 let storageRef = Storage.storage().reference().child("postImages/\(imageName)")
                 
                 storageRef.putData(imageData, metadata: nil) { metadata, error in
                     if let error = error {
-                        // Handle error
+                        observer.onNext(.handleError)
                     } else {
                         // 이미지 다운로드 URL 획득
                         storageRef.downloadURL { url, error in
@@ -93,7 +96,7 @@ class FireBaseService {
                                                 observer.onNext(.handleError)
                                             } else if let document = document, document.exists, let userName = document.data()?["name"] as? String {
                                                 // 이미지 URL과 함께 포스트 데이터 Firestore에 저장
-                                                let postData: [String: Any] = [
+                                                var postData: [String: Any] = [
                                                     "title": title,
                                                     "imageURL": url.absoluteString,
                                                     "profileURL": profileURL.absoluteString,
@@ -102,14 +105,27 @@ class FireBaseService {
                                                     "content": content,
                                                     "authorName": userName,
                                                     "authorID": currentUser.uid,
-                                                    "timestamp": FieldValue.serverTimestamp()
+                                                    "timestamp": FieldValue.serverTimestamp(),
+                                                    "likeVotes": [],
+                                                    "unlikeVotes": []
                                                 ]
                                                 
-                                                Firestore.firestore().collection("posts").addDocument(data: postData) { error in
+                                                // 포스트를 Firestore에 추가하고, 문서 ID를 가져와서 postData에 추가합니다.
+                                                let postRef = Firestore.firestore().collection("posts").document()
+                                                postRef.setData(postData) { error in
                                                     if let error = error {
                                                         observer.onNext(.handleError)
                                                     } else {
-                                                        observer.onNext(.success)
+                                                        // 문서 ID를 postData에 추가합니다.
+                                                        postData["id"] = postRef.documentID
+                                                        // Firestore에 다시 업데이트합니다.
+                                                        postRef.setData(postData) { error in
+                                                            if let error = error {
+                                                                observer.onNext(.handleError)
+                                                            } else {
+                                                                observer.onNext(.success)
+                                                            }
+                                                        }
                                                     }
                                                 }
                                             } else {
@@ -124,6 +140,54 @@ class FireBaseService {
                 }
             }
             return Disposables.create()
+        }
+    }
+    
+    
+    func vote(postId: String, vote: String) {
+        guard let userID = Auth.auth().currentUser?.uid else {
+            print("User not authenticated")
+            return
+        }
+
+        // Update like/unlike votes
+        let postRef = Firestore.firestore().collection("posts").document(postId)
+        postRef.getDocument { (document, error) in
+            guard let document = document, document.exists else {
+                print("Post document not found")
+                return
+            }
+            
+            var updatedPost = document.data() ?? [:]
+            var likeVotes = updatedPost["likeVotes"] as? [String] ?? []
+            var unlikeVotes = updatedPost["unlikeVotes"] as? [String] ?? []
+
+            switch vote {
+            case "like":
+                likeVotes.append(userID)
+                if let index = unlikeVotes.firstIndex(of: userID) {
+                    unlikeVotes.remove(at: index)
+                }
+            case "unlike":
+                unlikeVotes.append(userID)
+                if let index = likeVotes.firstIndex(of: userID) {
+                    likeVotes.remove(at: index)
+                }
+            default:
+                return
+            }
+
+            // Update Firestore
+            postRef.updateData([
+                "likeVotes": likeVotes,
+                "unlikeVotes": unlikeVotes
+            ]) { error in
+                if let error = error {
+                    print("Error updating document: \(error.localizedDescription)")
+                } else {
+                    print("Document successfully updated")
+                }
+            }
         }
     }
 }
