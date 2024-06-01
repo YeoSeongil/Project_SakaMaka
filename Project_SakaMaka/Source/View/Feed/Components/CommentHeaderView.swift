443d7bcdaed936fe0e6d3c724471a6c9b8b50080
YeoSeongil <seongil_yeo@naver.com>
2024-06-01 19:26:15 +0900

    [FEAT] FEAT CommentHeaderView

--- Project_SakaMaka/Source/View/Feed/Components/CommentHeaderView.swift
+++ Project_SakaMaka/Source/View/Feed/Components/CommentHeaderView.swift
@@ -24,14 +24,24 @@ class CommentHeaderView: UIView {
     
     // MARK: - UI Components
     
-    private let title = UILabel().then {
-        $0.text = "댓글"
-        $0.textColor = .black
-        $0.backgroundColor = .clear
-        $0.textAlignment = .center
-        $0.font = .h5
+    private let lineView = CustomLineView().then {
+        $0.lineColor = .nightGray
+        $0.lineWidth = 1.0
     }
     
+    private let title = UILabel().then {
+        let attributedString = NSMutableAttributedString(string: "댓글", attributes: [.font: UIFont.b1, .foregroundColor: UIColor.Turquoise])
+        attributedString.append(NSAttributedString(string: " 0", attributes: [.font: UIFont.b3, .foregroundColor: UIColor.black]))
+        $0.attributedText = attributedString
+    }
+//    private let title = UILabel().then {
+//        $0.text = "댓글"
+//        $0.textColor = .black
+//        $0.backgroundColor = .clear
+//        $0.textAlignment = .center
+//        $0.font = .h5
+//    }
+//    
     private let closeButton = UIButton().then {
         $0.setImage(UIImage(systemName: "xmark"), for: .normal)
         $0.tintColor = .black
@@ -54,7 +64,7 @@ class CommentHeaderView: UIView {
     private func setView() {
         backgroundColor = .clear
         
-        [title, closeButton].forEach { addSubview($0) }
+        [title, closeButton, lineView].forEach { addSubview($0) }
     }
     
     private func setConstraint() {
@@ -68,6 +78,10 @@ class CommentHeaderView: UIView {
             $0.trailing.equalToSuperview().inset(20)
         }
 
+        lineView.snp.makeConstraints {
+            $0.bottom.equalToSuperview()
+            $0.horizontalEdges.equalToSuperview()
+        }
     }
     
     private func bind() {
