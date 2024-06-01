de0ab785a859f2845247ee3935a160c4d1320d7a
YeoSeongil <seongil_yeo@naver.com>
2024-06-01 19:25:43 +0900

    [FEAT] FEAT CustomLineView

--- Project_SakaMaka/Configure/HeaderView/CustomLineView.swift
+++ Project_SakaMaka/Configure/HeaderView/CustomLineView.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class CustomLineView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    // 라인의 색상과 두께를 설정할 수 있는 변수
+    var lineColor: UIColor = .black
+    var lineWidth: CGFloat = 1.0
+    
+    private let shapeLayer = CAShapeLayer()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.setupLayer()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.setupLayer()
+    }
+    
+    private func setupLayer() {
+        self.layer.addSublayer(shapeLayer)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: self.bounds.height / 2))
+        path.addLine(to: CGPoint(x: self.bounds.width, y: self.bounds.height / 2))
+        
+        shapeLayer.path = path.cgPath
+        shapeLayer.strokeColor = lineColor.cgColor
+        shapeLayer.lineWidth = lineWidth
+    }
 }
