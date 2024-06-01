687dc8471af9519b59e5402a46b6d2ed7e765034
YeoSeongil <seongil_yeo@naver.com>
2024-06-01 18:44:32 +0900

    [ADD] ADD CustomLineView

---
+++ Project_SakaMaka/Configure/HeaderView/CustomLineView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomLineView.swift
+//  Project_SakaMaka
+//
+//  Created by 여성일 on 6/1/24.
+//
+
+import UIKit
+
+class CustomLineView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
