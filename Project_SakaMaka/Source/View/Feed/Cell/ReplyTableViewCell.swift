80123a02a86f8153668858caeef18e981c11e256
YeoSeongil <seongil_yeo@naver.com>
2024-06-03 20:47:02 +0900

    [FEAT] FEAT ReplyTableViewCell

--- Project_SakaMaka/Source/View/Feed/Cell/ReplyTableViewCell.swift
+++ Project_SakaMaka/Source/View/Feed/Cell/ReplyTableViewCell.swift
@@ -7,17 +7,122 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+
+import SnapKit
+import Then
+import FirebaseFirestore
+
 class ReplyTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let id: String = "ReplyTableViewCell"
+    
+    private let disposeBag = DisposeBag()
+    
+    // MARK: - UI Components
+    private let profileImageView = UIImageView().then {
+        $0.backgroundColor = .clear
+        $0.layer.cornerRadius = 17.5
+        $0.layer.masksToBounds = true
+        $0.image = UIImage(systemName: "person.crop.circle.fill")
+        $0.tintColor = .Turquoise
+    }
+    
+    private let userNameLabel = UILabel().then {
+        $0.backgroundColor = .clear
+        $0.font = .b6
+        $0.textColor = .black
+    }
+    
+    private let commentDateLabel = UILabel().then {
+        $0.backgroundColor = .clear
+        $0.font = .b7
+        $0.textColor = .nightGray
+        $0.numberOfLines = 0
+    }
+    
+    private let setupButton = UIButton().then {
+        $0.setImage(UIImage(systemName: "ellipsis" ), for: .normal)
+        $0.tintColor = .black
+    }
+    
+    private let commentLabel = UILabel().then {
+        $0.backgroundColor = .clear
+        $0.font = .b6
+        $0.textColor = .black
+        $0.numberOfLines = 0
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    // Init
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setCell()
+        setConstraint()
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: SetUp Cell
+    private func setCell() {
+        backgroundColor = .white
+        
+        [profileImageView, userNameLabel, commentDateLabel, setupButton, commentLabel].forEach {
+            self.contentView.addSubview($0)
+        }
+    }
+    
+    private func setConstraint() {
+        profileImageView.snp.makeConstraints {
+            $0.width.height.equalTo(35)
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalToSuperview().offset(50)
+        }
+        
+        userNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(10)
+        }
+        
+        commentDateLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.leading.equalTo(userNameLabel.snp.trailing).offset(5)
+        }
+        
+        commentLabel.snp.makeConstraints {
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(5)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(10)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().offset(-10)
+        }
+        
+        setupButton.snp.makeConstraints {
+            $0.top.equalToSuperview().offset(10)
+            $0.trailing.equalToSuperview().inset(20)
+        }
+    }
+    
+    private func bind() {
+      
+    }
+}
 
+extension ReplyTableViewCell {
+    func configuration(reply: Reply) {
+           profileImageView.setImageKingfisher(with: reply.authorProfileURL)
+           userNameLabel.text = reply.authorName
+           commentDateLabel.text = formatTimestamp(reply.timestamp)
+           commentLabel.text = reply.content
+       }
+}
+
+extension ReplyTableViewCell {
+    private func formatTimestamp(_ timestamp: Timestamp) -> String{
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd"
+        
+        let date = timestamp.dateValue()
+        return formatter.string(from: date)
+    }
 }
