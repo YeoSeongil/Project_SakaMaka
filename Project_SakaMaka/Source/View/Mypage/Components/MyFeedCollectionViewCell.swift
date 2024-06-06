6898995cf892071b92db9256a57ee8a511a23c16
YeoSeongil <seongil_yeo@naver.com>
2024-06-06 17:21:54 +0900

    [ADD] ADD MyFeedCollectionViewCell

---
+++ Project_SakaMaka/Source/View/Mypage/Components/MyFeedCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyFeedCollectionViewCell.swift
+//  Project_SakaMaka
+//
+//  Created by 여성일 on 6/6/24.
+//
+
+import UIKit
+
+class MyFeedCollectionViewCell: UICollectionViewCell {
+    
+}
