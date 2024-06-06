313ded411cfce9c0d7fb1f704429565403bcd267
YeoSeongil <seongil_yeo@naver.com>
2024-06-06 13:48:47 +0900

    [ADD] ADD MypageInfoView

---
+++ Project_SakaMaka/Source/View/Mypage/Components/MypageInfoView.swift
@@ -0,0 +1,20 @@
+//
+//  MypageInfoView.swift
+//  Project_SakaMaka
+//
+//  Created by 여성일 on 6/6/24.
+//
+
+import UIKit
+
+class MypageInfoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
