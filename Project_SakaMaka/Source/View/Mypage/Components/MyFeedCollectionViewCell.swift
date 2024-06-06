8e53f1d674f6d4883f7f638ad96085673a29ca47
YeoSeongil <seongil_yeo@naver.com>
2024-06-06 18:22:10 +0900

    [FEAT] FEAT MyFeedCollectionViewCell

--- Project_SakaMaka/Source/View/Mypage/Components/MyFeedCollectionViewCell.swift
+++ Project_SakaMaka/Source/View/Mypage/Components/MyFeedCollectionViewCell.swift
@@ -7,6 +7,49 @@
 
 import UIKit
 
+import Then
+import SnapKit
+
+import RxSwift
+import RxCocoa
+
 class MyFeedCollectionViewCell: UICollectionViewCell {
+    static let id: String = "MyFeedCollectionViewCell"
     
+    private let disposeBag = DisposeBag()
+    
+    //MARK: - UI Components
+    private let thumbnailImageView = UIImageView().then {
+        $0.contentMode = .scaleToFill
+        $0.tintColor = .milkWhite
+    }
+    
+    // MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCell()
+        setConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - Setup Cell
+    private func setCell() {
+        backgroundColor = .milkWhite
+        addSubview(thumbnailImageView)
+    }
+    
+    private func setConstraint() {
+        thumbnailImageView.snp.makeConstraints {
+            $0.verticalEdges.horizontalEdges.equalToSuperview()
+        }
+    }
+}
+
+extension MyFeedCollectionViewCell {
+    func configuration(url: Thumbnail) {
+        thumbnailImageView.setImageKingfisher(with: url.url)
+    }
 }
