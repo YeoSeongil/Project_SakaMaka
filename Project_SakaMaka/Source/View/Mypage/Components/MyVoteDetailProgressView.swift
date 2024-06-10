8f0a85ff9e05158160c5b569c6e08da1d58fa1e3
YeoSeongil <seongil_yeo@naver.com>
2024-06-10 14:33:20 +0900

    [ADD] ADD MyVoteDetailProgressView

---
+++ Project_SakaMaka/Source/View/Mypage/Components/MyVoteDetailProgressView.swift
@@ -0,0 +1,20 @@
+//
+//  MyVoteDetailProgressView.swift
+//  Project_SakaMaka
+//
+//  Created by 여성일 on 6/10/24.
+//
+
+import UIKit
+
+class MyVoteDetailProgressView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
